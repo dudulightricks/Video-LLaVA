aa9264d08b41c0703f2edcd67e653aa156f957b1
Dudu Moshe <dudu@lightricks.com>
2024-02-05 02:26:19 +0200

    lora train on manual

--- scripts/v1_5/finetune_lora.sh
+++ scripts/v1_5/finetune_lora.sh
@@ -1,14 +1,14 @@
 
-IMAGE_FOLDER="/opt/msr-vtt-for-train"
-VIDEO_FOLDER="/opt/msr-vtt-for-train"
+IMAGE_FOLDER="/opt/my-manual-for-train-video"
+VIDEO_FOLDER="/opt/my-manual-for-train-video"
 cd /opt/Video-LLaVA
 
 deepspeed videollava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed ./scripts/zero2_offload.json \
-    --model_name_or_path /opt/llava-video-7b \
+    --model_name_or_path /opt/llava-video-with-msrvtt \
     --version v1 \
-    --data_path /opt/msr-vtt-train-part.json \
+    --data_path /opt/my-manual-for-train-video.json \
     --image_folder ${IMAGE_FOLDER} \
     --image_tower LanguageBind/LanguageBind_Image \
     --video_folder ${VIDEO_FOLDER} \
@@ -20,14 +20,14 @@ deepspeed videollava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/videollava-7b-lora \
+    --output_dir ./checkpoints/videollava-7b-manual-lora \
     --num_train_epochs 2 \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 4 \
-    --evaluation_strategy "epoch" \
+    --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 500 \
+    --save_steps 50 \
     --save_total_limit 1 \
     --learning_rate 2e-4 \
     --weight_decay 0. \
