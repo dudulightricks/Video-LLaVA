ee4b51c15ac7cf184a0607b5f344f79bd494c0c9
Dudu Moshe <dudu@lightricks.com>
2024-03-26 12:06:06 +0200

    still save

--- videollava/eval/video/run_inference_video_qa.py
+++ videollava/eval/video/run_inference_video_qa.py
@@ -122,7 +122,14 @@ def run_inference(args):
     # gt_questions = get_chunk(gt_questions, args.num_chunks, args.chunk_idx)
     # gt_answers = json.load(open(args.gt_file_answers, "r"))
     # gt_answers = get_chunk(gt_answers, args.num_chunks, args.chunk_idx)
-    model.save_pretrained("/opt/llava-video-with-lora/")
+    model.save_pretrained("/opt/llava-video-gemini-73k-1epoch/")
+    torch.save(model.get_model().get_image_tower().state_dict(),
+               "/opt/tpu-llava-video-gemini-73k/llava/vision_tower_weights.pth")
+    torch.save(model.get_model().mm_projector.state_dict(),
+               "/opt/tpu-llava-video-gemini-73k/llava/mm_projector_weights.pth")
+    torch.save(model.get_model().get_video_tower().state_dict(),
+               "/opt/tpu-llava-video-gemini-73k/llava/video_tower_weights.pth")
+    torch.save(model.lm_head.state_dict(), "/opt/tpu-llava-video-gemini-73k/llava/lm_heads.pth")
     exit(0)
     answers_file = os.path.join(args.output_dir, f"{args.output_name}.json")
     os.makedirs(args.output_dir, exist_ok=True)
