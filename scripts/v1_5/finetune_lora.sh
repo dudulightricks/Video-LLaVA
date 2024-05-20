170dc7068e70eb847fa66efe079a758e6fed5589
Dudu Moshe <dudu@lightricks.com>
2024-05-20 22:19:20 +0300

    1 epoch of new 1.1 mil gemini

--- scripts/v1_5/finetune_lora.sh
+++ scripts/v1_5/finetune_lora.sh
@@ -1,14 +1,14 @@
 
-IMAGE_FOLDER="/opt/hdvila-with-gemini-clean-440k"
-VIDEO_FOLDER="/opt/hdvila-with-gemini-clean-440k"
+IMAGE_FOLDER="/opt/hdvila-with-gemini-1136k"
+VIDEO_FOLDER="/opt/hdvila-with-gemini-1136k"
 cd /opt/Video-LLaVA
 
 deepspeed videollava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed ./scripts/zero2_offload.json \
-    --model_name_or_path /opt/llava-video-gemini-337k-2epoch \
+    --model_name_or_path /opt/llava-video-gemini-440k-on-337k-2epoch \
     --version v1 \
-    --data_path /opt/hdvila-with-gemini-clean-440k-train.json \
+    --data_path /opt/hdvila-with-gemini-1136k-train.json \
     --image_folder ${IMAGE_FOLDER} \
     --image_tower LanguageBind/LanguageBind_Image \
     --video_folder ${VIDEO_FOLDER} \
@@ -20,8 +20,8 @@ deepspeed videollava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/videollava-hdvila-with-gemini-440k-lora-on-337k-2epoch-base \
-    --num_train_epochs 2 \
+    --output_dir ./checkpoints/videollava-hdvila-with-gemini-1136k-lora-on-777k \
+    --num_train_epochs 1 \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 4 \
