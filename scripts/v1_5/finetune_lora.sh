65b875ae20e9ebcc15ef953988315cc4f39ed5dc
Dudu Moshe <dudu@lightricks.com>
2024-11-21 15:02:20 +0200

    eran finetune

--- scripts/v1_5/finetune_lora.sh
+++ scripts/v1_5/finetune_lora.sh
@@ -1,14 +1,14 @@
 
-IMAGE_FOLDER="/opt/hdvila-with-gemini-1136k"
-VIDEO_FOLDER="/opt/hdvila-with-gemini-1136k"
+IMAGE_FOLDER="/opt/eran-data"
+VIDEO_FOLDER="/opt/eran-data"
 cd /opt/Video-LLaVA
 
 deepspeed videollava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
     --deepspeed ./scripts/zero2_offload.json \
-    --model_name_or_path /opt/llava-video-gemini-440k-on-337k-2epoch \
+    --model_name_or_path /opt/llava-video-gemini-dataset-mix-242k-on-1136k \
     --version v1 \
-    --data_path /opt/hdvila-with-gemini-1136k-train.json \
+    --data_path /opt/eran-data-train.json \
     --image_folder ${IMAGE_FOLDER} \
     --image_tower LanguageBind/LanguageBind_Image \
     --video_folder ${VIDEO_FOLDER} \
@@ -20,7 +20,7 @@ deepspeed videollava/train/train_mem.py \
     --image_aspect_ratio pad \
     --group_by_modality_length True \
     --bf16 True \
-    --output_dir ./checkpoints/videollava-hdvila-with-gemini-1136k-lora-on-777k \
+    --output_dir ./checkpoints/videollava-after-eran-finetune \
     --num_train_epochs 2 \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 1 \
--- videollava/train/train.py
+++ videollava/train/train.py
@@ -873,7 +873,7 @@ def make_supervised_data_module(tokenizer: transformers.PreTrainedTokenizer,
                                 data_path=data_args.data_path,
                                 data_args=data_args)
     eval_dataset = LazySupervisedDataset(tokenizer=tokenizer,
-                                data_path=["/opt/hdvila-with-gemini-1136k-val.json"],
+                                data_path=["/opt/eran-data-eval.json"],
                                 data_args=data_args)
     data_collator = DataCollatorForSupervisedDataset(tokenizer=tokenizer)
     return dict(train_dataset=train_dataset,
