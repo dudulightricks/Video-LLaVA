0f48134c1edb5bfd0f7513841ae0e71ad28c7ff3
Dudu Moshe <dudu@lightricks.com>
2024-11-21 21:24:14 +0200

    eran improve

--- videollava/train/train.py
+++ videollava/train/train.py
@@ -873,7 +873,7 @@ def make_supervised_data_module(tokenizer: transformers.PreTrainedTokenizer,
                                 data_path=data_args.data_path,
                                 data_args=data_args)
     eval_dataset = LazySupervisedDataset(tokenizer=tokenizer,
-                                data_path=["/opt/eran-data-short-eval.json"],
+                                data_path=["/opt/eran-data-improve-eval.json"],
                                 data_args=data_args)
     data_collator = DataCollatorForSupervisedDataset(tokenizer=tokenizer)
     return dict(train_dataset=train_dataset,
